a89a0ff732a0b1d909ff25a3c89962c9119d3c0d
Harshvardhan6078 <Basudeharsh@gmail.com>
2025-09-04 19:05:31 +0530

    basic python

---
+++ basic.py
@@ -0,0 +1,12 @@
+print('hello captain')
+a,d=2.2,1.2
+b=6.1
+c=a-d
+print(float(a-d))    
+print(type(a))           
+print(type(c))
+name1='hsagv'
+name2='''dhgvcg'''
+print(2*name1)
+print(4* name2)
+roll=int(input('please enter roll no'))
