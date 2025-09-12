d638ea6df874dd9c3aada67c7435c5034d03190b
Harshvardhan6078 <Basudeharsh@gmail.com>
2025-09-12 18:59:45 +0530

    practice file

---
+++ practice1.py
@@ -0,0 +1,13 @@
+'''# print("hello")
+fav1,fav2,fav3=(input('movie1'),input('movie2'),input('movie3'))
+print(fav1,fav2,fav3)
+list1=[fav1,fav2,fav3]
+print(type(list1))
+print(list1)
+'''
+#code for palindrome
+s=input('enter string')
+if s==s[::-1]:
+     print('palindrome')
+else:
+    print('not palindrome')
