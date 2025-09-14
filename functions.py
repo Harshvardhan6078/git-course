463f8486180db9f521639c7eb6bc73959b613552
Harshvardhan6078 <Basudeharsh@gmail.com>
2025-09-14 23:19:45 +0530

    dictionary practice

---
+++ functions.py
@@ -0,0 +1,16 @@
+
+# function defination
+def sum(a,b):
+    return a+b
+
+# function call
+sum= sum(1,2)
+print(sum)
+
+#--------------------------------
+def average(a,b,c):
+    average=(a+b+b)/3
+    return average
+    
+average1=average(2,3,4)
+print(average1)
